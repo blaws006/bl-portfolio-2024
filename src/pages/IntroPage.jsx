242526db292615d1654c0f1c58a081c9a6658369
blaws006 <blaws006@gmail.com>
2024-05-05 14:06:02 -0400

    Starting profile section of my porfolio page

--- src/pages/IntroPage.jsx
+++ src/pages/IntroPage.jsx
@@ -1,8 +1,19 @@
-
+import brandon from '../assets/Daniel Headshot v2.png'
 
 const IntroPage = () => {
   return (
-    <div>IntroPage</div>
+    <main>
+      <h1 className='text-center my-10 text-3xl font-bold flex-column'>Profile</h1>
+      <section className='sm:container sm:flex justify-evenly content-center columns-1 mx-auto'>
+        <div className='profileImg'>
+          <img src={brandon} className='aspect-square mx-auto' />
+        </div>
+        <div className='profileDesc text-center sm:text-left'>
+            <p>Name: Brandon Lawson</p>
+        </div>
+      </section>
+     
+    </main>
   )
 }
 
