f32416cf8a5822db4442e231368a2a6330f4dd17
blaws006 <blaws006@gmail.com>
2024-07-24 11:53:18 -0400

    Fixed form button in mobile view

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -16,7 +16,7 @@ const ContactForm = () => {
 				<label htmlFor='message'>Message</label>
 				<br />
 				<Field name='message' as='textarea' className='border-2 block mb-8 h-auto text-lg resize-y'></Field>
-				<button type='submit' className='bg-black hover:bg-white text-white hover:text-black hover: border-black hover:border-x-2 hover:border-y-2 p-4 w-2/12 self-center hover:scale-125 ease-out duration-300'>
+				<button type='submit' className='bg-black hover:bg-white text-white hover:text-black hover: border-black hover:border-x-2 hover:border-y-2 p-4 sm:w-2/6 lg:w-2/12 self-center hover:scale-125 ease-out duration-300'>
 					Submit
 				</button>
 			</Form>
