7f610b94fe4f57c4b47876faa816b1e098509b1a
blaws006 <blaws006@gmail.com>
2024-07-23 20:47:57 -0400

    Is this correct?

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,9 @@ const App = () => {
 		<div className='flex flex-col min-h-screen justify-between'>
 			<Header />
 			<Routes>
-				<Route path='/bl-portfolio-2024/' element={<IntroPage />} />
-				<Route path='/bl-portfolio-2024/portfolio' element={<PortfolioPage />} />
-				<Route path='/bl-portfolio-2024/contact' element={<ContactPage />} />
+				<Route path='bl-portfolio-2024/' element={<IntroPage />} />
+				<Route path='bl-portfolio-2024/portfolio' element={<PortfolioPage />} />
+				<Route path='bl-portfolio-2024/contact' element={<ContactPage />} />
 			</Routes>
 			<Footer />
 		</div>
