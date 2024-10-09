ac285188df82a8e94edf4a69f56e071f9a37a482
blaws006 <blaws006@gmail.com>
2024-10-08 20:14:34 -0400

    Swapped out profile image to something more agreeable to being rounded

--- src/pages/IntroPage.jsx
+++ src/pages/IntroPage.jsx
@@ -1,4 +1,4 @@
-import brandon from '../assets/Daniel Headshot v2.png';
+import brandon from '../assets/Brandon-Lawson_Ferguson_Kim-Brundage-Photography_062322610---20220715.png';
 
 const IntroPage = () => {
 	return (
@@ -6,7 +6,7 @@ const IntroPage = () => {
 			<h1 className='text-center my-10 text-3xl font-bold flex-column'>Profile</h1>
 			<section className='sm:container sm:flex justify-evenly content-center columns-1 mx-auto'>
 				<div className='profileImg'>
-					<img src={brandon} className='aspect-square mx-auto' />
+					<img src={brandon} className=' mx-auto' />
 				</div>
 				<div className='profileDesc text-center sm:text-left'>
 					<p>Name: Brandon Lawson</p>
