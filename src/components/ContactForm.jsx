bd881c6aec826257569263c9f8076a51f60f2daf
blaws006 <blaws006@gmail.com>
2024-11-07 22:02:36 -0500

    Added Modal on Submission

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -3,8 +3,11 @@ import { validateContactForm } from '../utils/validateContactForm';
 import { useState } from 'react';
 import Loading from './Loading';
 
+import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react';
+
 const ContactForm = () => {
 	const [loading, setLoading] = useState(false);
+	const [open, setOpen] = useState(true);
 	const handleSubmit = async (values, { resetForm }) => {
 		setLoading(true);
 
@@ -27,8 +30,10 @@ const ContactForm = () => {
 		}).then((res) => res.json());
 
 		if (res.success) {
+			setOpen(true);
 			console.log('Success', res);
 			setLoading(false);
+
 			resetForm();
 		}
 	};
@@ -44,6 +49,50 @@ const ContactForm = () => {
 			validate={validateContactForm}>
 			<Form className='flex flex-col align-middle content-center mx-auto sm:container my-32'>
 				{loading && <Loading />}
+				{open && (
+					<>
+						<Dialog
+							open={open}
+							onClose={setOpen}
+							className='relative z-10'>
+							<DialogBackdrop
+								transition
+								className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in'
+							/>
+
+							<div className='fixed inset-0 z-10 w-screen overflow-y-auto'>
+								<div className='flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0'>
+									<DialogPanel
+										transition
+										className='relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95'>
+										<div className='bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4'>
+											<div className='sm:flex sm:items-start'>
+												<div className='mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left'>
+													<DialogTitle
+														as='h3'
+														className='text-base font-semibold text-gray-900'>
+														Thank You!
+													</DialogTitle>
+													<div className='mt-2'>
+														<p className='text-sm text-gray-500'>{`Your message has been sent! I'll get back to you as soon as possible.`}</p>
+													</div>
+												</div>
+											</div>
+										</div>
+										<div className='bg-gray-50 px-4 py-3 sm:flex sm:flex-row justify-center sm:px-6'>
+											<button
+												type='button'
+												onClick={() => setOpen(false)}
+												className='inline-flex w-full justify-center rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-white hover:text-black hover:border-black hover:border-2 sm:ml-3 sm:w-auto'>
+												Close
+											</button>
+										</div>
+									</DialogPanel>
+								</div>
+							</div>
+						</Dialog>
+					</>
+				)}
 				<label htmlFor='email'>Email Address</label> <br />
 				<Field
 					name='email'
